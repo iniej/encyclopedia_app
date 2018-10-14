257f924ed59e295e48a0aed78213c3384a16f263
xong <xong@email.com>
2018-10-14 03:22:17 -0500

    Created flask forms that will be converted to html forms

---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class SignupForm(FlaskForm):
+
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField('Login')
+
+
+class SearchForm(FlaskForm):
+
+    search = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Search')
