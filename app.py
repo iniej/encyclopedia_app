64e8ca6a6197305b88f26a0baf184d594068c57f
xong <xong@email.com>
2018-10-14 00:22:00 -0500

    Added title param

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('signup.html', title='Sign up')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='Log in')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -26,13 +26,13 @@ def home(user=None):
 @app.route('/<user>/result', methods=['POST', 'GET'])
 def result(user=None):
 
-    if request.method == "POST":
+    if request.method == 'POST':
         search_text = request.form['search']
         if search_text != "":
             image_url = APIs.PixabayAPI.get_image_url(search_text)
             description = APIs.WikiAPI.get_description(search_text)
             return render_template('home.html', image_url=image_url,
-                                   description=description, search_text=search_text, user=user)
+                                   description=description, search_text=search_text, user=user, title='Search result')
 
 
 if __name__ == '__main__':
